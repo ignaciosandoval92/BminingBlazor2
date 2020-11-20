7643f12d9136a7bfb149c6d3ac87e52746c004c3
Ignacio Sandoval <ignacio.sandoval@bmining.cl>
2020-11-20 11:32:24 -0300

    cambio al nuevo servidor

--- Data/TableConstants.cs
+++ Data/TableConstants.cs
@@ -8,13 +8,13 @@ namespace Data
 {
     public class TableConstants
     {
-        public const string TablaUsuario = "test.Usuario";
-        public const string TablaContrato = "test.Contrato";
-        public const string TablaProyecto = "test.Proyecto";
-        public const string TablaTipoProyecto = "test.Tipo_Proyecto";
-        public const string TablaTipoEstadoPago = "test.Tipo_EstadoPago";
-        public const string TablaEstadoPago = "test.EstadoPago";
-        public const string TablaIntegrantes = "test.Integrantes_Proyecto";
-        public const string TablaClientes = "test.Cliente";
+        public const string TablaUsuario = "bminin39_SGPBM.Usuario";
+        public const string TablaContrato = "bminin39_SGPBM.Contrato";
+        public const string TablaProyecto = "bminin39_SGPBM.Proyecto";
+        public const string TablaTipoProyecto = "bminin39_SGPBM.Tipo_Proyecto";
+        public const string TablaTipoEstadoPago = "bminin39_SGPBM.Tipo_EstadoPago";
+        public const string TablaEstadoPago = "bminin39_SGPBM.EstadoPago";
+        public const string TablaIntegrantes = "bminin39_SGPBM.Integrantes_Proyecto";
+        public const string TablaClientes = "bminin39_SGPBM.Cliente";
     }
 }
