9281d5ed0d316bf42b368711f5c09378ae908258
Ignacio Sandoval <ignacio.sandoval@bmining.cl>
2020-11-27 14:43:02 -0300

    agregada algunas variables al view model

--- BminingBlazor/ViewModels/Projects/ProjectViewModel.cs
+++ BminingBlazor/ViewModels/Projects/ProjectViewModel.cs
@@ -1,4 +1,5 @@
-﻿using BminingBlazor.ViewModels.User;
+﻿using System;
+using BminingBlazor.ViewModels.User;
 using System.Collections.Generic;
 
 namespace BminingBlazor.ViewModels.Projects
@@ -9,12 +10,15 @@ namespace BminingBlazor.ViewModels.Projects
         public string ProjectCode { get; set; }
         public string ProjectName { get; set; }
         public ProjectStatusEnum ProjectStatus { get; set; }
+        public ProjectTypeEnum ProjectType { get; set; }
         public List<PaymentViewModel> Payments { get; set; }
         public UserViewModel ProjectManager { get; set; }
         public UserViewModel Creator { get; set; }
         public List<MemberViewModel> Members { get; set; }
         public string ClientName { get; set; }
         public int ClientId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
        
 
         public ProjectViewModel()
