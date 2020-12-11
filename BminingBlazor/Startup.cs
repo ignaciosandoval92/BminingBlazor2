fb0e6167798d8c62c5bc00a8c723865f52854dbe
Ignacio Sandoval <ignacio.sandoval@bmining.cl>
2020-12-11 11:46:58 -0300

    sin http context

--- BminingBlazor/Startup.cs
+++ BminingBlazor/Startup.cs
@@ -33,8 +33,7 @@ namespace BminingBlazor
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddHttpContextAccessor();
+        {        
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddTransient<IDataAccess, DataAccess>();
