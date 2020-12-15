b57602354feaecfdf96522f3fd1190a19bcc99d4
Ignacio Sandoval <ignacio.sandoval@bmining.cl>
2020-12-15 20:12:08 -0300

    Update SendGridConstants.cs

--- BminingBlazor/Utility/SendGridConstants.cs
+++ BminingBlazor/Utility/SendGridConstants.cs
@@ -5,4 +5,8 @@
         public const string ApiKey = "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";
         public const string SenderEmail = "francisco.soto@bmining.cl";
     }
+
 }
+
+
+
