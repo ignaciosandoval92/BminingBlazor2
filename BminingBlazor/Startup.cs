39e683a2f07820d664ad938b2be8ebd0d37422d9
Ignacio Sandoval <ignacio.sandoval@bmining.cl>
2020-12-29 13:17:36 -0300

    avanze paginacion proyectos

--- BminingBlazor/Startup.cs
+++ BminingBlazor/Startup.cs
@@ -34,7 +34,7 @@ namespace BminingBlazor
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {        
+        {           
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddTransient<IDataAccess, DataAccess>();
