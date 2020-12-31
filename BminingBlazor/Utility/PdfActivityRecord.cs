e5708950a6b0027f8173f4cf12246aeb678df222
Ignacio Sandoval <ignacio.sandoval@bmining.cl>
2020-12-31 11:00:08 -0300

    .

--- BminingBlazor/Utility/PdfActivityRecord.cs
+++ BminingBlazor/Utility/PdfActivityRecord.cs
@@ -7,15 +7,26 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Blazored.TextEditor;
 namespace BminingBlazor.Utility
 {
     public class PdfActivityRecord
     {
+        private BlazoredTextEditor Notes;
+        public string Note { get; set; }
+        public async Task<string> OnitianalizedAsync()
+        {
+            await Notes.LoadHTMLContent(_oActivityRecord.MyNotes);
+            var Note = await Notes.GetText();
+            return Note;
+        }
         #region Declararion
         int _maxColumn = 3;
         Document _document;
         PdfPTable _pdfTable = new PdfPTable(3);
+        PdfPTable _pdfTable2 = new PdfPTable(3);
+        PdfPTable _pdfTable3 = new PdfPTable(4);
+        PdfPTable _pdfTable4 = new PdfPTable(3);
         PdfPCell _pdfCell;
         Font _fontStyle;
         MemoryStream _memoryStream = new MemoryStream();
@@ -27,6 +38,12 @@ namespace BminingBlazor.Utility
             _document = new Document(PageSize.A4, 10f, 10f, 20f, 30f);
             _pdfTable.WidthPercentage = 100;
             _pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfTable2.WidthPercentage = 100;
+            _pdfTable2.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfTable3.WidthPercentage = 100;
+            _pdfTable3.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfTable4.WidthPercentage = 100;
+            _pdfTable4.HorizontalAlignment = Element.ALIGN_LEFT;
             _fontStyle = FontFactory.GetFont("Arial", 8f, 1);
             PdfWriter.GetInstance(_document, _memoryStream);
             _document.Open();
@@ -40,9 +57,16 @@ namespace BminingBlazor.Utility
 
             this.ReportHeader();
             this.ReportBody();
+            this.ReportBody2();
+            this.ReportBody3();
+            this.ReportBody4();
 
             _pdfTable.HeaderRows = 2;
+            //_pdfTable2.HeaderRows = 2;
             _document.Add(_pdfTable);
+            _document.Add(_pdfTable2);
+            _document.Add(_pdfTable3);
+            _document.Add(_pdfTable4);
             _document.Close();
             return _memoryStream.ToArray();
         }
@@ -56,8 +80,9 @@ namespace BminingBlazor.Utility
             _pdfCell.ExtraParagraphSpace = 0;
             _pdfTable.AddCell(_pdfCell);
             _pdfTable.CompleteRow();
+            _document.Add(Chunk.Newline);
 
-            _fontStyle = FontFactory.GetFont("Arial", 14f, 1);
+            _fontStyle = FontFactory.GetFont("Arial", 12f, 1);
             _pdfCell = new PdfPCell(new Phrase($"{Resource.Project}:{_oActivityRecord.MyProjectCode}-{_oActivityRecord.MyProjectName}", _fontStyle));
             _pdfCell.Colspan = _maxColumn;
             _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
@@ -65,8 +90,9 @@ namespace BminingBlazor.Utility
             _pdfCell.ExtraParagraphSpace = 0;
             _pdfTable.AddCell(_pdfCell);
             _pdfTable.CompleteRow();
+            _document.Add(Chunk.Newline);
 
-            _fontStyle = FontFactory.GetFont("Arial", 14f, 1);
+            _fontStyle = FontFactory.GetFont("Arial", 12f, 1);
             _pdfCell = new PdfPCell(new Phrase($"{Resource.Date}:{_oActivityRecord.MyDate}", _fontStyle));
             _pdfCell.Colspan = _maxColumn;
             _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
@@ -74,7 +100,50 @@ namespace BminingBlazor.Utility
             _pdfCell.ExtraParagraphSpace = 0;
             _pdfTable.AddCell(_pdfCell);
             _pdfTable.CompleteRow();
+            _document.Add(Chunk.Newline);
+
+            _fontStyle = FontFactory.GetFont("Arial", 12f, 1);
+            _pdfCell = new PdfPCell(new Phrase($"{Resource.DurationHours}:{_oActivityRecord.MyDurationHours}", _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _document.Add(Chunk.Newline);
+
+            _fontStyle = FontFactory.GetFont("Arial", 12f, 1);
+            _pdfCell = new PdfPCell(new Phrase($"{Resource.Place}:{_oActivityRecord.MyPlace}", _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _document.Add(Chunk.Newline);
+
+            _fontStyle = FontFactory.GetFont("Arial", 10f, 1);
+            _pdfCell = new PdfPCell(new Phrase(Resource.Members, _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _document.Add(Chunk.Newline);
+
+            _fontStyle = FontFactory.GetFont("Arial", 1f, 1);
+            _pdfCell = new PdfPCell(new Phrase(" ", _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _document.Add(Chunk.Newline);
+
         }
+
 
         private void ReportBody()
         {
@@ -104,8 +173,8 @@ namespace BminingBlazor.Utility
             #endregion
 
             #region Table Body
-           
-            foreach(var oActivityRecord in _oActivityRecord.OurMembers)
+
+            foreach (var oActivityRecord in _oActivityRecord.OurMembers)
             {
                 _pdfCell = new PdfPCell(new Phrase(oActivityRecord.MyName, fontStyle));
                 _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
@@ -126,6 +195,185 @@ namespace BminingBlazor.Utility
                 _pdfTable.AddCell(_pdfCell);
             }
             #endregion
+        }
+        private void ReportBody2()
+        {
+            _fontStyle = FontFactory.GetFont("Arial", 1f, 1);
+            _pdfCell = new PdfPCell(new Phrase(" ", _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable2.AddCell(_pdfCell);
+            _pdfTable2.CompleteRow();
+            _document.Add(Chunk.Newline);
+
+            _fontStyle = FontFactory.GetFont("Arial", 10f, 1);
+            _pdfCell = new PdfPCell(new Phrase(Resource.Commitments, _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable2.AddCell(_pdfCell);
+            _pdfTable2.CompleteRow();
+            _document.Add(Chunk.Newline);
+
+            _fontStyle = FontFactory.GetFont("Arial", 1f, 1);
+            _pdfCell = new PdfPCell(new Phrase(" ", _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable2.AddCell(_pdfCell);
+            _pdfTable2.CompleteRow();
+            _document.Add(Chunk.Newline);
+
+        }
+        private void ReportBody3()
+        {
+            _fontStyle = FontFactory.GetFont("Arial", 9f, 1);
+            var fontStyle = FontFactory.GetFont("Arial", 9f, 0);
+
+            #region Table Header
+            _pdfCell = new PdfPCell(new Phrase(Resource.Responsible, _fontStyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.Gray;
+            _pdfTable3.AddCell(_pdfCell);
+
+            _pdfCell = new PdfPCell(new Phrase(Resource.EndDate, _fontStyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.Gray;
+            _pdfTable3.AddCell(_pdfCell);
+
+            _pdfCell = new PdfPCell(new Phrase(Resource.Commitment, _fontStyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.Gray;
+            _pdfTable3.AddCell(_pdfCell);
+
+            _pdfCell = new PdfPCell(new Phrase(Resource.Status, _fontStyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.Gray;
+            _pdfTable3.AddCell(_pdfCell);
+
+            _pdfTable.CompleteRow();
+            #endregion
+
+            #region Table Body
+
+            foreach (var oActivityRecord in _oActivityRecord.OurCommitments)
+            {
+                _pdfCell = new PdfPCell(new Phrase(oActivityRecord.MyResponsible, fontStyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.White;
+                _pdfTable3.AddCell(_pdfCell);
+
+                _pdfCell = new PdfPCell(new Phrase(oActivityRecord.MyCommitmentDate.ToString("dd/MM/yyyy"), fontStyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.White;
+                _pdfTable3.AddCell(_pdfCell);
+
+                _pdfCell = new PdfPCell(new Phrase(oActivityRecord.MyCommitment, fontStyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.White;
+                _pdfTable3.AddCell(_pdfCell);
+
+                _pdfCell = new PdfPCell(new Phrase(oActivityRecord.MyStatus.ToString(), fontStyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.White;
+                _pdfTable3.AddCell(_pdfCell);
+            }
+            #endregion
+        }
+        private void ReportBody4()
+        {
+            _fontStyle = FontFactory.GetFont("Arial", 1f, 1);
+            _pdfCell = new PdfPCell(new Phrase(" ", _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable4.AddCell(_pdfCell);
+            _pdfTable4.CompleteRow();
+            _document.Add(Chunk.Newline);
+
+            _fontStyle = FontFactory.GetFont("Arial", 10f, 1);
+            _pdfCell = new PdfPCell(new Phrase(Resource.SecurityReflection, _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable4.AddCell(_pdfCell);
+            _pdfTable4.CompleteRow();
+            _document.Add(Chunk.Newline);
+
+            _fontStyle = FontFactory.GetFont("Arial", 1f, 1);
+            _pdfCell = new PdfPCell(new Phrase(" ", _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable4.AddCell(_pdfCell);
+            _pdfTable4.CompleteRow();
+            _document.Add(Chunk.Newline);
+
+            _fontStyle = FontFactory.GetFont("Arial", 7f, 0);
+            _pdfCell = new PdfPCell(new Phrase(_oActivityRecord.MySecurityReflection.Replace("<p>", "").Replace("</p>", ""), _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable4.AddCell(_pdfCell);
+            _pdfTable4.CompleteRow();
+            _document.Add(Chunk.Newline);
+
+            _fontStyle = FontFactory.GetFont("Arial", 1f, 1);
+            _pdfCell = new PdfPCell(new Phrase(" ", _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable4.AddCell(_pdfCell);
+            _pdfTable4.CompleteRow();
+            _document.Add(Chunk.Newline);
+
+            _fontStyle = FontFactory.GetFont("Arial", 10f, 1);
+            _pdfCell = new PdfPCell(new Phrase(Resource.Notes, _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable4.AddCell(_pdfCell);
+            _pdfTable4.CompleteRow();
+            _document.Add(Chunk.Newline);
+
+            _fontStyle = FontFactory.GetFont("Arial", 1f, 1);
+            _pdfCell = new PdfPCell(new Phrase(" ", _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable4.AddCell(_pdfCell);
+            _pdfTable4.CompleteRow();
+            _document.Add(Chunk.Newline);
+
+            
+            _fontStyle = FontFactory.GetFont("Arial", 7f, 0);
+            _pdfCell = new PdfPCell(new Phrase(Note, _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable4.AddCell(_pdfCell);
+            _pdfTable4.CompleteRow();
+            _document.Add(Chunk.Newline);
         }
     }
 }
