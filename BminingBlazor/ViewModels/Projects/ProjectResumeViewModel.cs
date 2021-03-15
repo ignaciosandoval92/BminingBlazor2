42c652d50c50aa495fc37723412400cf877fd954
Ignacio Sandoval <ignacio.sandoval@bmining.cl>
2021-03-15 16:11:57 -0300

    sistema busqueda en reportes

--- BminingBlazor/ViewModels/Projects/ProjectResumeViewModel.cs
+++ BminingBlazor/ViewModels/Projects/ProjectResumeViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace BminingBlazor.ViewModels.Projects
+﻿using System;
+
+namespace BminingBlazor.ViewModels.Projects
 {
     public class ProjectResumeViewModel
     {
         public int MyProjectId { get; set; }
         public string MyProjectCode { get; set; }
         public string MyProjectName { get; set; }
+        public DateTime MyStartDate { get; set; }
+        public DateTime MyEndDate { get; set; }
     }
 }
